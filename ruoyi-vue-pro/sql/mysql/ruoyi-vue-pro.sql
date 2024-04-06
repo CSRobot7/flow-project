1fc6ad7da601e92e49bb60c8e937e93bd261f4f8
AIRobot <AIRobot@feature.com>
2024-04-06 21:20:44 +0800

    commit

--- ruoyi-vue-pro/sql/mysql/ruoyi-vue-pro.sql
+++ ruoyi-vue-pro/sql/mysql/ruoyi-vue-pro.sql
@@ -5823,3 +5823,21 @@ BEGIN;
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+CREATE TABLE `bpm_category`  (
+                                 `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+                                 `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '组名',
+                                 `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '描述',
+                                 `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '描述',
+                                 `status` tinyint NOT NULL COMMENT '状态（0正常 1停用）',
+                                 `sort` tinyint NOT NULL COMMENT '分类排序',
+                                 `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '创建者',
+                                 `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                 `updater` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '更新者',
+                                 `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                 `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
+                                 `tenant_id` bigint NOT NULL DEFAULT 0 COMMENT '租户编号',
+                                 PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 113 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '流程分类';
+
